55fbf43fe0d6ca218af0893d180d3fc15714ac40
wongal5 <a.wong9970@gmail.com>
2018-02-19 22:16:18 -0800

    progress on twiceLinear

---
+++ twiceLinear.js
@@ -0,0 +1,17 @@
+function dblLinear(n) {
+  let arr = [1], i = 0, tracker = {};
+  while (arr.length < n * Math.pow(n, 1/3)) {
+    arr.push(2 * arr[i] + 1);
+    arr.push(3 * arr[i] + 1);
+    i++;
+  }
+  arr.forEach(num => { tracker[num] = num; })
+  return parseInt(Object.values(tracker).sort((a, b) => a-b)[n]);
+}
+
+console.log(dblLinear(10), 22);
+console.log(dblLinear(20), 57);
+console.log(dblLinear(30), 91);
+console.log(dblLinear(50), 175);
+console.log(dblLinear(100), 447);
+console.log(dblLinear(100000), 447);
